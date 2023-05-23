64506a472ef36e642abb040326179f41c5df13b5
kryuk1980 <kruyk@bk.ru>
2023-05-23 22:39:15 +0300

    Задание выполнено

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,22 @@
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.onprogress = function (event) {
+        let total = event.total;
+        let loaded = event.loaded;
+        let delta = loaded / total;
+
+        progress.value = delta;
+    }
+
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+
+    const formData = new FormData(form);
+    xhr.send(formData);
+})
